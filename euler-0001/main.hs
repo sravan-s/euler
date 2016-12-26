17195c673f894d6472ece9512f52304d1861f82e
ess_ess <sravans2011@gmail.com>
2016-12-26 23:33:10 +0530

    Implement basic logic
    To Do:
    * Implement sigma operation

--- euler-0001/main.hs
+++ euler-0001/main.hs
@@ -3,3 +3,18 @@
 -- ------------------------------------------------------------------------
 -- Find the sum of all the multiples of 3 or 5 below 1000
 
+findN large small = quot large small
+
+noOfThrees = findN 999 3
+noOfFives = findN 999 5
+
+sumOfFirstN n = (n * (n + 1)) / 2
+
+multiplyThree n = n * 3
+multiplyFive n = n * 5
+
+findSumOfMultiplesOfThree = sigma (findN 999 3) (sumOfFirstN multiplyThree)
+
+-- Implement sigma operation
+-- sigma count func =
+
