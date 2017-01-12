0c565fcbd053342f8a63419f1a14a2df02dc5d34
sravan <sravan.s@flytxt.com>
2017-01-12 17:30:00 +0530

    [WIP] :space_invader: Sieve of Eratosthenes

--- euler-0007/nthPrime.hs
+++ euler-0007/nthPrime.hs
@@ -2,3 +2,12 @@
 
 -- What is the 10 001st prime number?
 
+-- https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+largest = maxBound :: Int
+
+allNums = [2 .. largest]
+
+isDivisible num arr = filter (\x ->  x `mod` num == 0) arr
+
+primes = map (\num -> isDivisible num allNums) allNums
+
