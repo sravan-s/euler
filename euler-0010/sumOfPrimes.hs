60df5f159bf05ccea3800441edf55934ff7fd703
sravan <sravan.s@flytxt.com>
2017-01-16 18:10:47 +0530

    Problem definition for euler0010, euler0011

--- euler-0010/sumOfPrimes.hs
+++ euler-0010/sumOfPrimes.hs
@@ -8,8 +8,16 @@ primes = sieve [2 ..]
   where
     sieve (p:xs) = p : sieve [ x | x <- xs, x `mod` p > 0]
 
+-- http://stackoverflow.com/a/3596543
+primes' :: [Int]
+primes' = 2: 3: sieve (tail primes') [5,7..]
+ where
+  sieve (p:ps) xs = h ++ sieve ps [x | x <- t, x `rem` p /= 0]
+                                -- or:  filter ((/=0).(`rem`p)) t
+                  where (h,~(_:t)) = span (< p*p) xs
+
 nPrimes :: Int -> [Int]
-nPrimes n = takeWhile (< n) primes
+nPrimes n = takeWhile (< n) primes'
 
 sumOfArr' = foldl (+) 0
 
